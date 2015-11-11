20087472b1cef2823b85443228eee1fa5fc2fe41
Antti Halla <antti.halla@gmail.com>
2015-11-11 11:34:20 +0200

    Add head scripts, static files

--- server.rkt
+++ server.rkt
@@ -17,21 +17,21 @@
        (post id body slug date_published title)))
 
 
-
-
-
-(define head-scripts '("jquery-2.1.1.min.js" "boostrap.min.js" "mousetrap.min.js"))
+(define head-scripts '("/static/jquery-2.1.1.min.js" 
+                       "/static/bootstrap-3.2.0-dist/js/bootstrap.min.js" 
+                       "/static/mousetrap.min.js"))
 
 (define head-styles '("http://fonts.googleapis.com/css?family=Raleway"
                       "http://fonts.googleapis.com/css?family=Lustria" 
                       "/static/bootstrap-3.2.0-dist/css/bootstrap.min.css"
-                      "static/style.css"))
+                      "/static/style.css"))
 
 (define (page-head)
   `(head
     (meta [[http-equiv "Content-type"] [content "text/html; charset=utf-8"]])
     (meta [[name "viewport"] [content "width=device-width, initial-scale=1"]])
     ,@(map (λ (x) `(link [[type "text/css"] [rel "stylesheet"] [href ,x]])) head-styles)
+    ,@(map (λ (x) `(script [[type "text/javascript"] [src ,x]])) head-scripts)
 
     (title "Antti Halla")))
 
